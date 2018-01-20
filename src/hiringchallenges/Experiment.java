4ffd3f46dc2bf22d55f7914eba33c8be6e22e76b
Arpit Agrawal <arpit.agrawal@webklipper.com>
2018-01-20 22:59:39 +0530

    Got AC in KilJee and XOR problem

--- src/hiringchallenges/Experiment.java
+++ src/hiringchallenges/Experiment.java
@@ -1,20 +1,10 @@
 package hiringchallenges;
 
-import java.util.Comparator;
-import java.util.PriorityQueue;
-
 /**
  * Created by bugkiller on 8/5/2017.
  */
 public class Experiment {
     public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        pq.offer(5);
-        pq.offer(4);
-        pq.offer(3);
-        pq.offer(2);
-        pq.offer(1);
-        pq.offer(0);
-        System.out.println(pq);
+        System.out.println(PayuKilljeeAndSubsets.exp(2, 1));
     }
 }
