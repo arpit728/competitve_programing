0276addae7cc4a8fc2dee2454515b632cec85444
Arpit Agrawal <arpit.agrawal@webklipper.com>
2018-05-07 12:30:56 +0530

    Power of matrix road repairment

--- cp-playlist/src/main/java/com/duotech/cp/PetyaAndRepairmentOfRoads.java
+++ cp-playlist/src/main/java/com/duotech/cp/PetyaAndRepairmentOfRoads.java
@@ -1,4 +1,102 @@
 package com.duotech.cp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+
+import static java.lang.Integer.parseInt;
+
 public class PetyaAndRepairmentOfRoads {
+    static int edges[][] = new int[200001][3];
+    static int parent[] = new int[100001];
+    static boolean milkMen[] = new boolean[100001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n, m, source, t, p;
+        String s[];
+        t = parseInt(br.readLine());
+        while (t-- > 0) {
+            s = br.readLine().split("\\s");
+            n = parseInt(s[0]);
+            m = parseInt(s[1]);
+            s = br.readLine().split("\\s");
+            for (int i = 1; i <= n; i++) {
+                if ("1".equals(s[i - 1])) {
+                    milkMen[i] = true;
+                }
+            }
+            for (int i = 0; i < m; i++) {
+                s = br.readLine().split("\\s");
+                edges[i][0] = parseInt(s[0]);
+                edges[i][1] = parseInt(s[1]);
+                edges[i][2] = parseInt(s[2]);
+                if (milkMen[edges[i][0]] && milkMen[edges[i][1]]) {
+                    edges[i][2] = 0;
+                }
+            }
+            System.out.println(solve(n, m));
+        }
+    }
+
+    private static String solve(int n, int m) {
+        HashSet<Integer> nonMilkmenConnectedCities = new HashSet<>();
+        Arrays.sort(edges, 0, m, Comparator.comparingInt(o -> o[2]));
+        for (int i = 1; i <=n; i++) {
+            parent[i] = i;
+            nonMilkmenConnectedCities.add(i);
+        }
+        long ans = 0;
+        for (int i = 0; i < m; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            if (findSet(u) != findSet(v)) {
+                ans += edges[i][2];
+                findUnion(u, v,nonMilkmenConnectedCities);
+            }
+        }
+        for (int i = 1; i <= n; i++) {
+            findSet(i);
+            if (milkMen[parent[i]]) {
+                nonMilkmenConnectedCities.remove(i);
+            }
+        }
+        if (nonMilkmenConnectedCities.size() > 0) {
+            return "impossible";
+        }
+
+        return ans + "";
+    }
+
+    private static void findUnion(int u, int v, HashSet<Integer> nonMilkmenConnectedCities) {
+
+        if (milkMen[u]) {
+            parent[findSet(v)] = findSet(u);
+        } else {
+            parent[findSet(u)] = findSet(v);
+        }
+    }
+
+    private static int findSet(int x) {
+
+        if (x != parent[x]) {
+            parent[x] = findSet(parent[x]);
+        }
+        return parent[x];
+    }
+
 }
+/*
+1
+5 7
+0 1 0 1 0
+1 2 11
+1 3 1
+1 5 17
+2 3 1
+3 5 18
+4 5 3
+2 4 5
+*/
--- cp-playlist/src/main/java/com/duotech/cp/PowerOfMatrix.java
+++ cp-playlist/src/main/java/com/duotech/cp/PowerOfMatrix.java
@@ -1,4 +1,69 @@
 package com.duotech.cp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static java.lang.Integer.parseInt;
+
 public class PowerOfMatrix {
+
+    private static final long M = 1000000007;
+    static long x[][] = new long[50][50];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t, m, n;
+        String s[];
+        t = parseInt(br.readLine());
+        while (t-- > 0) {
+            s = br.readLine().split("\\s");
+            m = parseInt(s[0]);
+            n = parseInt(s[1]);
+            for (int i = 0; i < m; i++) {
+                s = br.readLine().split("\\s");
+                for (int j = 0; j < m; j++) {
+                    x[i][j] = parseInt(s[j]);
+                }
+            }
+            long ans[][] = solve(x,m, n);
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < m; j++) {
+                    System.out.print(ans[i][j]+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    private static long[][] solve(long[][] x, int m, int n) {
+
+        return pow(x,m,n);
+    }
+
+    private static long[][] pow(long[][] x, int m, int n) {
+
+        if (n == 1) {
+            return x;
+        }
+        long[][] temp = pow(x, m, n / 2);
+        temp = matrixMul(temp, temp, m);
+        if (n % 2 == 1) {
+            temp = matrixMul(x, temp, m);
+        }
+        return temp;
+    }
+
+    private static long[][] matrixMul(long[][] x, long[][] y, int m) {
+        long ans[][] = new long[m][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < m; k++) {
+                    ans[i][j] = ((ans[i][j] % M) + (((x[i][k]) % M) * ((y[k][j] % M)) % M)) % M;
+                }
+            }
+        }
+        return ans;
+    }
 }
